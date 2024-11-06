ee7dc1f4919c55a32709d7e35fbffdf4d2e5f633
Siva1325 <97665069+Siva1325@users.noreply.github.com>
2024-11-06 14:20:39 +0530

    Add files via upload

--- Prediction.py
+++ Prediction.py
@@ -3,6 +3,9 @@ from streamlit_extras.let_it_rain import rain
 from streamlit_extras.colored_header import colored_header
 import pandas as pd
 import pickle
+from PIL import Image
+
+st.image('C:\\Users\\Nandhini Sivaramanan\\capstone-project\\CarDekho_capstone\\CarDekho-FY22-social.jpg', caption="Cardekho_prediction", use_column_width=True)
 
 def app():
     colored_header(
